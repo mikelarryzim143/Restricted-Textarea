c4aa11b6a1846adf4b9004034e9d1d16be659ba0
mikelarryzim143 <58751184+mikelarryzim143@users.noreply.github.com>
2025-02-22 10:43:57 +0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function countCharacters(){
     const textarea= document.getElementById("textarea");
-    const cont=document.getElementsById("content");
+    
     const charCount=textarea.value.length;
-    document.getElementById("charCounts").innerText=charCount + "/250";
+    document.getElementById("charCounts").innerText=charCount + " /250";
+    if (parseInt(charCount)==250){
+        document.getElementById("content").className=
+        document.getElementById("content").className.replace("", " invalid ");
+        document.getElementById("textarea").className=
+        document.getElementById("textarea").className.replace("", " invalidd ")
+    }
 
 }
